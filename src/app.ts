c9a73c3784724cc4ee111ca9c7c8a966fe4341c4
Ladislav Dokulil <alladdin@zemres.cz>
2026-05-12 17:27:08 +0200

    Integrate Handlebars templates for HTML rendering, add index route, and update relay routes and project configuration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import relayRoutes from './routes/relayRoutes.ts';
+import indexRoutes from './routes/indexRoutes.ts';
+import path from "node:path";
+import { create } from 'express-handlebars';
 
 const app = express();
 
-app.use(express.json());
+const hbs = create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(import.meta.dirname, '../views'));
 
 // Routes
 app.use('/api/relay', relayRoutes);
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from IrrigationPI server!' });
-});
+app.use('/', indexRoutes);
 
 export default app;
--- src/routes/relayRoutes.ts
+++ src/routes/relayRoutes.ts
@@ -1,7 +1,9 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import {getRelay, getRelays, setAllRelays, setRelay} from "../controllers/relayController.ts";
 
 const router = Router();
+
+router.use(express.json());
 
 router.get('/', getRelays);
 router.get('/:id', getRelay);
